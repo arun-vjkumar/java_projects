bdec8a0fe235d74934472c5f7fcd9874bc4ff5ab
Arun <arunvrvce@gmail.com>
2019-11-01 23:56:09 +0530

    some more thread details

--- JavaMultithreadingConcepts/src/main/java/threadInfo/ThreadDetails.java
+++ JavaMultithreadingConcepts/src/main/java/threadInfo/ThreadDetails.java
@@ -9,5 +9,8 @@ public class ThreadDetails {
         logger.info("Thread Details Is As Follows");
         logger.info(String.format("Thread Id: %d", thread.getId()));
         logger.info(String.format("Thread Name: %s", thread.getName()));
+        logger.info(String.format("Thread Priority Level: %d", thread.getPriority()));
+        logger.info(String.format("Thread State: %s", thread.getState()));
+        logger.info(String.format("Thread Group: %s",thread.getThreadGroup()));
     }
 }
